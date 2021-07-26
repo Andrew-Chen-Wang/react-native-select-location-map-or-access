8231d8bf6ded23ae2deb189006490da5f38385bb
Andrew-Chen-Wang <acwangpython@gmail.com>
2021-07-26 00:09:35 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,51 @@
+/*
+Copyright 2021 Andrew Chen Wang
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiYWN3YW5ncHl0aG9uIiwiYSI6ImNrcjdlOGZ4YjIzZ24ybnJ6OWx0eHI4ZnoifQ.yq_cR-h1RDf0AnYVNVhMtA',
+);
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapboxGL.MapView style={styles.map} logoEnabled={false} />
+        </View>
+      </View>
+    );
+  }
+}
